84fd63d437f5e0fca0f3e5eb6b004ae193ea2065
Atul Aryan <imatularyan@gmail.com>
2024-06-28 07:52:51 +0530

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,43 +1,46 @@
-// _document.js
-
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import { ColorModeScript } from "@chakra-ui/react";
-import theme from "../lib/theme";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+import Script from 'next/script';
+import theme from '../lib/theme';
 
 const jsonLd = {
   "@context": "https://schema.org",
   "@type": "Person",
-  name: "Atul Aryan",
-  jobTitle: "Freelance Full-stack Developer",
-  url: "https://www.atularyan.online/",
-  sameAs: [
+  "name": "Atul Aryan",
+  "jobTitle": "Freelance Full-stack Developer",
+  "url": "https://www.atularyan.online/",
+  "sameAs": [
     "https://github.com/imatularyan",
     "https://www.linkedin.com/in/atularyan/",
-    "https://twitter.com/Imatularyan",
-    "https://instagram.com/Imatularyan",
-    "https://facebook.com/Imatularyan",
+    "https://twitter.com/Imatularyan"
   ],
-  worksFor: {
+  "worksFor": {
     "@type": "Organization",
-    name: "Freelancer",
+    "name": "Freelancer"
   },
-  description:
-    "Atul Aryan, a freelance full-stack developer based in India, specializing in HTML, CSS, JavaScript, React, EJS, Parel, Next.js, Three.js, Python, Tailwind CSS, Chakra UI, Vite, Redux, React Native, MongoDB, Jest, Node.js, and more. Visit for portfolio and contact information.",
+  "description": "Atul Aryan, a freelance full-stack developer based in India, specializing in HTML, CSS, JavaScript, React, EJS, Parel, Next.js, Three.js, Python, Tailwind CSS, Chakra UI, Vite, Redux, React Native, MongoDB, Jest, Node.js, and more. Visit for portfolio and contact information."
 };
 
-export default class Document extends NextDocument {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            name="description"
-            content="Atul Aryan, a freelance full-stack developer based in India, specializing in HTML, CSS, JavaScript, React, EJS, Parel, Next.js, Three.js, Python, Tailwind CSS, Chakra UI, Vite, Redux, React Native, MongoDB, Jest, Node.js, and more. Visit for portfolio and contact information."
+          <meta name="description" content="Atul Aryan, a freelance full-stack developer based in India, specializing in HTML, CSS, JavaScript, React, EJS, Parel, Next.js, Three.js, Python, Tailwind CSS, Chakra UI, Vite, Redux, React Native, MongoDB, Jest, Node.js, and more. Visit for portfolio and contact information." />
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XHPN86NW6K"></Script>
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-XHPN86NW6K');
+              `
+            }}
           />
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-          />
+          <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
